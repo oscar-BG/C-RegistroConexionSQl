2d9cd65fbc597e13ef44d4e10759345a1da338b3
Oscar Bautista <oscar04262000@gmail.com>
2020-11-22 21:15:35 -0600

    Creamos una conexion de SQL y C# esta conexion comprueba si el usuario existe, en caso de que exista se abre un segundo formulario

--- appConexion/appUsuario/Form1.cs
+++ appConexion/appUsuario/Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace appUsuario
 {
     public partial class Form1 : Form
     {
+        FBienvenido f2 = new FBienvenido();
+        SqlConnection cadenaConexion = new SqlConnection("Data Source=LAPTOP-OBEL3V1L;Initial Catalog=Sabatino;Integrated Security=True");
         public Form1()
         {
             InitializeComponent();
@@ -19,15 +22,26 @@ namespace appUsuario
 
         private void btnAceptar_Click(object sender, EventArgs e)
         {
-            if(txtPassword.Text != "" && txtUsuario.Text != "")
+            String cadenaSql = "select * from usuarios where nombreUsuario = " + "'" + txtUsuario.Text + "' and clave = " + "'" + txtPassword.Text + "'";
+
+            SqlCommand comando = new  SqlCommand(cadenaSql, cadenaConexion);
+            cadenaConexion.Open();
+            SqlDataReader lector = comando.ExecuteReader();
+            if (lector.Read() == true)
             {
-                if (txtPassword.Text == "Hola" && txtUsuario.Text == "Hol@")
-                {
-                    FBienvenido f2 = new FBienvenido();
-                    this.Hide();
-                    f2.ShowDialog();
-                }
+                this.Hide();
+                f2.ShowDialog();
+
             }
+            else
+            {
+                MessageBox.Show("Usuario o Password incorrecto");
+                txtUsuario.Clear();
+                txtPassword.Clear();
+                txtUsuario.Focus();
+            }
+
+            cadenaConexion.Close();
         }
     }
 }
