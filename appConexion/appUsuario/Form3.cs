ea4ee8c63697b76ae5396acac8426b6b9d121428
Oscar Bautista <oscar04262000@gmail.com>
2020-11-28 23:54:27 -0600

    Realizamos la conexion y verificamos si la matricula es duplicada, si la matricula es duplicada ya no se registra si no lo es ya se registra

--- appConexion/appUsuario/Form3.cs
+++ appConexion/appUsuario/Form3.cs
@@ -50,54 +50,82 @@ namespace appUsuario
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
+            
         }
 
         private void btnAceptar_Click(object sender, EventArgs e)
         {
+            SqlConnection cadanaConexionUsu = new SqlConnection("Data Source=LAPTOP-OBEL3V1L; Initial Catalog= ClaseSql; Integrated Security= True");
+
             if (txtCarrera.Text == "" || cbxSituacion.SelectedIndex < 0 || txtPaterno.Text == "" ||
                 txtMaterno.Text == "" || txtNombres.Text == "" || cbxSexo.SelectedIndex < 0 ||
                 txtLNacimiento.Text == "" || txtCPostal.Text == "" || txtNacionalidad.Text == "" || txtEdad.Text == "")
             {
                 MessageBox.Show("Faltan campo por rellenar");
+                cadanaConexionUsu.Close();
             }
             else
             {
-                SqlConnection cadanaConexion = new SqlConnection("Data Source=LAPTOP-OBEL3V1L; Initial Catalog= ClaseSql; Integrated Security= True");
-                cadanaConexion.Open();
-                if (cbxSexo.SelectedIndex == 0)
-                    sexotemp = "M";
-                else if (cbxSexo.SelectedIndex == 1)
-                    sexotemp = "F";
-                fechanactemp = dtpFechaNac.Value.Date.ToString("dd/MM/yyy");
-                if (cbxSituacion.SelectedIndex == 0)
-                    situaciontemp = "A";
-                else if (cbxSituacion.SelectedIndex == 1)
-                    situaciontemp = "B";
-                else if (cbxSituacion.SelectedIndex == 2)
-                    situaciontemp = "T";
-                else if (cbxSituacion.SelectedIndex == 3)
-                    situaciontemp = "P";
+                
+                //Matriucla duplicada
+                String cadenaSqlMatricula = "select * from DatosPersonales where matricula = " + "'" + txtCarrera.Text +"'";
 
-                mayusculas(txtCarrera);
-                mayusculas(txtPaterno);
-                mayusculas(txtMaterno);
-                mayusculas(txtNombres);
-                mayusculas(txtLNacimiento);
-                mayusculas(txtNacionalidad);
 
-                String cadenaSql = "Insert into DatosPersonales" +
-                    "(Matricula, situacion, Paterno, Materno, Nombres, Sexo, FechaNacimiento," +
-                    "Lnacimiento, CP, Nacionalidad)" +
-                    "Values ('" + txtCarrera.Text + "','" + situaciontemp + "','" + txtPaterno.Text + "','" +
-                    txtMaterno.Text + "','" + txtNombres.Text + "','" +
-                    sexotemp + "','" + fechanactemp + "','" + txtLNacimiento.Text + "','" +
-                    txtCPostal.Text + "','" + txtNacionalidad.Text + "')";
-                SqlCommand comando = new SqlCommand(cadenaSql, cadanaConexion);
-                comando.ExecuteNonQuery();
-                cadanaConexion.Close();
-                MessageBox.Show("El registro se realizo con exito", "Registro realizado");
-                limpiar();
-                this.Close();
+                SqlCommand comando1 = new SqlCommand(cadenaSqlMatricula, cadanaConexionUsu);
+                cadanaConexionUsu.Open();
+                SqlDataReader lector = comando1.ExecuteReader();
+                if (lector.Read() == true)
+                {
+                    MessageBox.Show("La matricula ya existe");
+                    limpiar();
+                }
+                else
+                {
+
+                    SqlConnection cadanaConexion = new SqlConnection("Data Source=LAPTOP-OBEL3V1L; Initial Catalog= ClaseSql; Integrated Security= True");
+                    cadanaConexion.Open();
+                    if (cbxSexo.SelectedIndex == 0)
+                        sexotemp = "M";
+                    else if (cbxSexo.SelectedIndex == 1)
+                        sexotemp = "F";
+                    fechanactemp = dtpFechaNac.Value.Date.ToString("dd/MM/yyy");
+                    if (cbxSituacion.SelectedIndex == 0)
+                        situaciontemp = "A";
+                    else if (cbxSituacion.SelectedIndex == 1)
+                        situaciontemp = "B";
+                    else if (cbxSituacion.SelectedIndex == 2)
+                        situaciontemp = "T";
+                    else if (cbxSituacion.SelectedIndex == 3)
+                        situaciontemp = "P";
+
+                    mayusculas(txtCarrera);
+                    mayusculas(txtPaterno);
+                    mayusculas(txtMaterno);
+                    mayusculas(txtNombres);
+                    mayusculas(txtLNacimiento);
+                    mayusculas(txtNacionalidad);
+
+                   
+                    String cadenaSql = "Insert into DatosPersonales" +
+                        "(Matricula, situacion, Paterno, Materno, Nombres, Sexo, FechaNacimiento," +
+                        "Lnacimiento, CP, Nacionalidad)" +
+                        "Values ('" + txtCarrera.Text + "','" + situaciontemp + "','" + txtPaterno.Text + "','" +
+                        txtMaterno.Text + "','" + txtNombres.Text + "','" +
+                        sexotemp + "','" + fechanactemp + "','" + txtLNacimiento.Text + "','" +
+                        txtCPostal.Text + "','" + txtNacionalidad.Text + "')";
+
+                    SqlCommand comando = new SqlCommand(cadenaSql, cadanaConexion);
+                    comando.ExecuteNonQuery();
+                    cadanaConexion.Close();
+                    MessageBox.Show("El registro se realizo con exito", "Registro realizado");
+                    limpiar();
+                    this.Close();
+                }
+
+                cadanaConexionUsu.Close();
+
+
+                //Fin matricula duplicada
             }
         }
 
